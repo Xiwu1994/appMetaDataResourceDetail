e11fcff38f06f6c23979761c77895d37d42fa491
xiwu <xiwu@yunniao.me>
2017-11-10 11:50:54 +0800

    1

--- src/main/scala/appMetaDataResourceDetail/AppMetaDataResourceDetail.scala
+++ src/main/scala/appMetaDataResourceDetail/AppMetaDataResourceDetail.scala
@@ -235,9 +235,9 @@ object AppMetaDataResourceDetail {
           app_meta_data_resource_detail(e(0), e(1), e(2), e(3), e(4), e(5), today)
         })
       }).toDF().write
-      .mode(SaveMode.Append)
-      .format("parquet")
       .partitionBy("p_day")
+      .mode(SaveMode.Overwrite)
+      .format("parquet")
       .saveAsTable("app_meta_data.app_meta_data_resource_detail")
   }
 }
